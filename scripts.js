9c5ca2bedc262b908fab45c3ad2c3f243465e433
KHONGTUB <jkojeda12@gmail.com>
2022-03-05 20:47:47 -0600

    Pushed Dummy todo list

---
+++ scripts.js
@@ -0,0 +1,45 @@
+
+console.log('sanity check')
+
+let arrayOfTodos = [
+{
+    "userId": 14,
+    "id": 1,
+    "title": "I will not waste chalk.",
+    "completed": false
+},
+{
+    "userId": 20,
+    "id": 2,
+    "title": "this is the second title",
+    "completed": true
+}]
+
+const fetchTodos = () => {
+    fetch('https://reqres.in/api/users')
+    // ('https://jsonplaceholder.typicode.com/todos')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+        const ol = document.getElementById('todo-list')
+        ol.innerHTML = 
+        `
+            <img src="${data.data[0].avatar}">
+        `
+    })
+}
+
+const logTodos = () => {
+    console.log(arrayOfTodos)
+}
+
+const populateTodos = () => {
+    for (let i = 0; i < arrayOfTodos.length; i++) {
+        const ol = document.getElementById('todo-list')
+        let li = document.createElement('LI')
+        li.appendChild(document.createTextNode(arrayOfTodos[i].title))
+        ol.appendChild(li)
+    }
+}
